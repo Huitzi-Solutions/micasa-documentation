439c78768e781643dac9b59b220a1e9a17467f7a
0496natgs <garcia.natalia@iexe.edu.mx>
2021-10-30 19:31:18 -0600

    Add Google fonts link to HEAD in Config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,6 +22,7 @@ module.exports = config({
       "script",
       { src: "https://cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js" },
     ],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap' }],
   ],
 
   locales: {
